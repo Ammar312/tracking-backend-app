c765802c397c1fa2b6f2950a53559df5e28bd4fd
Ammar ul mustafa <ammar.ul.mustafa3@gmail.com>
2025-03-01 11:29:48 +0500

    setup

--- app.mjs
+++ app.mjs
@@ -1,9 +1,13 @@
 import express from "express";
+import { Server } from "socket.io";
+import http from "http";
 const app = express();
 const PORT = 3000;
+const server = http.createServer(app);
+const io = new Server(server);
 
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-app.listen(PORT, () => console.log(`PORT is running on ${PORT}`));
+server.listen(PORT, () => console.log(`PORT is running on ${PORT}`));
